06897dae817e5d4c34fbffdb0ace2bda7c747c89
Anselmis Columna <anselmiscolumna@gmail.com>
2026-01-02 16:09:45 -0500

    Updated the CSharp Grading project

--- Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
+++ Guided-project-foreach-if-array-CSharp-main/GuidedProject/Starter/Program.cs
@@ -1,62 +1,45 @@
-﻿using System;
-
-// initialize variables - graded assignments 
+﻿// initialize variables - graded assignments 
 int currentAssignments = 5;
-
-// String Names
-string[] studentNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
-
-foreach (string name in studentNames)
-{
-    Console.WriteLine($"{name}");
-}
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
 int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
+// Student names
+string[] studentNames = new string[] {"Sophia", "Andrew", "Emma", "Logan"};
+int[] studentScores = new int[10];
 
-decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
-
-foreach (int score in sophiaScores)
+foreach (string name in studentNames)
 {
-    sophiaSum += score;
+    string currentStudent = name;
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sophiaSum = 0;
+    
+    decimal sophiaScore;
+
+    foreach (int score in sophiaScores)
+    {
+        // add the exam score to the sum
+        sophiaSum += score;
+    }
+
+    sophiaScore = (decimal)(sophiaSum) / currentAssignments;
+
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 }
-
-foreach (int score in andrewScores)
-{
-    andrewSum += score;
-}
-
-foreach (int score in emmaScores)
-{
-    emmaSum += score;
-}
-
-foreach (int score in loganScores)
-{
-    loganSum += score;
-}
-
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
-
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-Console.WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-Console.WriteLine("Emma:\t\t" + emmaScore + "\tB");
-Console.WriteLine("Logan:\t\t" + loganScore + "\tA-");
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
-
