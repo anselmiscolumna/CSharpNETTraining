37724f10827145595ecdf3e61f4912682f6d9605
Anselmis Columna <anselmiscolumna@gmail.com>
2026-01-04 17:54:52 -0500

    Commit the Challenge proect for Student Grades

--- MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
+++ MyConsoleApp/obj/Debug/net10.0/MyConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2291a287f8c444e2cedaa539e9a9bd999b8f18e8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07d11331ec83ddeea46f11cc9756dd4e1b032681")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
