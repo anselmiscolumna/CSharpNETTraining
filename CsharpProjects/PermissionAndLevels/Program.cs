40f3d3608baf45f28b64a1a811f2d323a1069143
Anselmis Columna <anselmiscolumna@gmail.com>
2026-01-08 13:27:21 -0500

    Committing completed Permissions and Levels logic project.

--- CsharpProjects/PermissionAndLevels/Program.cs
+++ CsharpProjects/PermissionAndLevels/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Permission and Levels exercise
+
+string permission = "User";
+int level = 55;
+
+if ((!permission.Contains("Admin") && !permission.Contains("Manager")) || (permission.Contains("Manager") && level < 20))
+{
+    Console.WriteLine("You do not have sufficient priveleges.");
+}
+
+else if (permission.Contains("Admin"))
+{
+    Console.WriteLine(level > 55? "Welcome, Super Admin user." : "Welcome, Admin user.");
+}
+
+else if (permission.Contains("Manager") && level >= 20)
+{
+    Console.WriteLine("Contact an admin for access.");
+}
