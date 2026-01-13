40b4a7cef05a996395f43421092fb0b412278cd0
Anselmis Columna <anselmiscolumna@gmail.com>
2026-01-12 19:18:56 -0500

    Committing modified code for TestProject, loop logic, condition logic and do-while logic

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -328,6 +328,7 @@ Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
 
 /* ***Code Block and Variable Demo*** */
 
+/*
 bool flag = true;
 int value = 0;
 
@@ -339,5 +340,351 @@ if (flag)
 value = 10;
 Console.WriteLine($"Outside the code block: {value}");
 
+if(flag) Console.WriteLine(flag);
+
+string name = "steve";
+
+if (name == "bob") 
+  Console.WriteLine("Found Bob");
+else if (name == "steve") 
+  Console.WriteLine("Found Steve");
+else 
+  Console.WriteLine("Found Chuck");
+
+*/
 // ====================================================================================================================================================
 
+/* ***Code Challenge for updating Problematic Code */
+
+/*
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+      found = true;
+}
+
+if (found) 
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
+*/
+
+// ====================================================================================================================================================
+
+/* ***Switch Statement Demo*** */
+
+/*
+switch (fruit)
+{
+  case "apple":
+    Console.WriteLine($"App will display information for apple.");
+    break;
+  
+  case "banana":
+    Console.WriteLine($"App will display information for banana");
+    break;
+
+  case "cherry":
+    Console.WriteLine($"App will display information for cherry.");
+    break;
+}
+*/
+
+// ====================================================================================================================================================
+
+/* ***Employee Level and Name Demo*** */
+
+/*
+int employeeLevel = 300;
+string employeeName = "John Smith";
+
+string title = "";
+
+switch (employeeLevel)
+{
+  case 100:
+  case 200:
+    title = "Senior Associate";
+    break;
+
+  case 300:
+    title = "Manager";
+    break;
+
+  case 400:
+    title = "Senior Manager";
+    break;
+
+  default:
+    title = "Associate";
+    break;
+}
+
+Console.WriteLine($"{employeeName}, {title}");
+*/
+
+// ====================================================================================================================================================
+
+/* ***Convert if-elseif-else blocks into Switch Case*** */
+
+
+// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+// string sku = "01-MN-L";
+
+// string[] product = sku.Split('-');
+
+// string type = "";
+// string color = "";
+// string size = "";
+
+/* Switch Case Blocks
+switch (product[0])
+{
+  case "01":
+    type = "Sweat shirt"; 
+    break;
+
+  case "02":
+    type = "T-Shirt";
+    break;
+
+  case "03":
+    type = "Sweat pants";
+    break;
+  
+  default:
+    type = "Other";
+    break;
+}
+
+switch (product[1])
+{
+  case "BL":
+    color = "Black";
+    break;
+
+  case "MN":
+    color = "Maroon";
+    break;
+
+  default:
+    color = "White";
+    break;
+}
+
+switch (product[2])
+{
+  case "S":
+    size = "Small";
+    break;
+
+  case "M":
+    size = "Medium";
+    break;
+
+  case "L":
+    size = "Large";
+    break;
+
+  default:
+    size = "One Size Fits All";
+    break;
+}
+
+*/
+
+/* If-elseif-else Blocks
+if (product[0] == "01")
+{
+    type = "Sweat shirt";
+} else if (product[0] == "02")
+{
+    type = "T-Shirt";
+} else if (product[0] == "03")
+{
+    type = "Sweat pants";
+}
+else
+{
+    type = "Other";
+}
+
+if (product[1] == "BL")
+{
+    color = "Black";
+} else if (product[1] == "MN")
+{
+    color = "Maroon";
+} else
+{
+    color = "White";
+}
+
+if (product[2] == "S")
+{
+    size = "Small";
+} else if (product[2] == "M")
+{
+    size = "Medium";
+} else if (product[2] == "L")
+{
+    size = "Large";
+} else
+{
+    size = "One Size Fits All";
+}
+*/
+// Console.WriteLine($"Product: {size} {color} {type}");
+
+// ====================================================================================================================================================
+
+/* *** Foreach and For Loop Demos */
+
+/*
+for (int i = 0; i < 10; i++)
+{
+  Console.WriteLine(i);
+}
+
+for (int i = 10; i >= 0; i--)
+{
+  Console.WriteLine(i);
+}
+
+for (int i = 0; i < 10; i += 3)
+{
+  Console.WriteLine(i);
+}
+
+for (int i = 0; i < 10; i++)
+{
+  Console.WriteLine(i);
+  if (i == 7) break;
+}
+
+string[] names = {"Alex", "Eddie", "David", "Michael"};
+for (int i = names.Length - 1; i >= 0; i--)
+{
+  Console.WriteLine(names[i]);
+}
+
+for (int i = 0; i < names.Length; i++)
+  if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
+
+*/
+
+// ====================================================================================================================================================
+
+/* ***Code Challenge FizzBuzz For Loops*** */
+
+/*
+for (int i = 0; i <= 100; i++)
+{
+  if (i % 3 == 0 && i % 5 == 0)
+    Console.WriteLine($"{i} - FizzBuzz");
+
+  else if (i % 3 == 0)
+    Console.WriteLine($"{i} - Fizz");
+
+  else if (i % 5 == 0)
+    Console.WriteLine($"{i} - Buzz");
+
+  else
+    Console.WriteLine(i);
+}
+*/
+
+// ====================================================================================================================================================
+
+/* ***Do While Loop Demo*** */
+/*
+Random random = new Random();
+int current = random.Next(1, 11);
+
+
+do
+{
+  current = random.Next(1, 11);
+
+  if (current >= 8) continue;
+
+  Console.WriteLine(current);
+} while (current != 7);
+
+*/
+/*
+while (current >= 3)
+{
+  Console.WriteLine(current);
+  current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+*/
+
+// ====================================================================================================================================================
+
+/* ***Role Playing Hero Game (While - Do While Loop Challenge)*** */
+
+Random rand = new Random();
+int heroHealth = 10;
+int monsterHealth = 10;
+int attack = 0;
+
+string winner = "";
+string turn = "Hero";
+
+do
+{
+
+  attack = rand.Next(1, 11);
+  winner = turn;
+
+  switch (turn)
+  {
+    case "Hero":
+      monsterHealth -= attack;
+      Console.WriteLine($"Monster was damaged and lost {attack} health and now has {monsterHealth}.");
+      turn = "Monster";
+      break;
+
+    case "Monster":
+      heroHealth -= attack;
+      Console.WriteLine($"Hero was damaged and lost {attack} health and now has {heroHealth}.");
+      turn = "Hero";
+      break;
+  }
+
+} while(monsterHealth > 0 && heroHealth > 0);
+
+Console.WriteLine($"{winner} wins!");
+
+int hero = 10;
+int monster = 10;
+
+Random dice = new Random();
+
+do
+{
+    int roll = dice.Next(1, 11);
+    monster -= roll;
+    Console.WriteLine($"Monster was damaged and lost {roll} health and now has {monster} health.");
+
+    if (monster <= 0) continue;
+
+    roll = dice.Next(1, 11);
+    hero -= roll;
+    Console.WriteLine($"Hero was damaged and lost {roll} health and now has {hero} health.");
+
+} while (hero > 0 && monster > 0);
+
+Console.WriteLine(hero > monster ? "Hero wins!" : "Monster wins!");
