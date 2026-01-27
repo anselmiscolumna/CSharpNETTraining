1e8e661cc2e8f8e9bf5e5b4339bb9db2815fcae3
Anselmis Columna <anselmiscolumna@gmail.com>
2026-01-26 19:47:52 -0500

    Commiting all changes for TestProject learning and guided project case 2

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -701,55 +701,78 @@ string? readResult;
 int numericValue = 0;
 bool validNumber = false;
 
-Console.WriteLine("Enter an integer value between 5 and 10 ");
+// Console.WriteLine("Enter an integer value between 5 and 10 ");
 
-do
-{
-  readResult = Console.ReadLine();
-  if (readResult != null)
-  {
-    validNumber = int.TryParse(readResult, out numericValue);
-    if (validNumber == true)
-    {
-      if(!(numericValue > 5 && numericValue < 10))
-        Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
-    }
-    else
-      Console.WriteLine("Sorry, you entered an invalid number, please try again");
-  }
-} while (!(numericValue > 5 && numericValue < 10));
-Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
+// do
+// {
+//   readResult = Console.ReadLine();
+//   if (readResult != null)
+//   {
+//     validNumber = int.TryParse(readResult, out numericValue);
+//     if (validNumber == true)
+//     {
+//       if(!(numericValue > 5 && numericValue < 10))
+//         Console.WriteLine($"You entered {numericValue}. Please enter a number between 5 and 10.");
+//     }
+//     else
+//       Console.WriteLine("Sorry, you entered an invalid number, please try again");
+//   }
+// } while (!(numericValue > 5 && numericValue < 10));
+// Console.WriteLine($"Your input value ({numericValue}) has been accepted.");
 
-Console.WriteLine($"Enter your role name (Administrator, Manager, or User)");
+// Console.WriteLine($"Enter your role name (Administrator, Manager, or User)");
 
-bool validEntry = false;
-string role = "";
+// bool validEntry = false;
+// string role = "";
 
-do
-{
-  readResult = Console.ReadLine();
-  if (readResult != null)
-  {
-    role = readResult.Trim().ToLower();
+// do
+// {
+//   readResult = Console.ReadLine();
+//   if (readResult != null)
+//   {
+//     role = readResult.Trim().ToLower();
 
-    if(role == "administrator" || role == "manager" || role == "user")
-      validEntry = true;
+//     if(role == "administrator" || role == "manager" || role == "user")
+//       validEntry = true;
 
-    else
-      Console.WriteLine($"The role name that you entered, \"{role}\" is not valid. Enter your role name (Administrator, Manager, or User)");
-  }
-}while(!validEntry);
-Console.WriteLine($"Your input value ({role}) has been accepted.");
+//     else
+//       Console.WriteLine($"The role name that you entered, \"{role}\" is not valid. Enter your role name (Administrator, Manager, or User)");
+//   }
+// }while(!validEntry);
+// Console.WriteLine($"Your input value ({role}) has been accepted.");
 
-string[] myStrings = new string[2] {"I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices"};
+string[] myStrings = new string[2] { "I like pizza. I like roast chicken. I like salad", "I like all three of the menu choices" };
+int stringsCount = myStrings.Length;
+
+string myString = "";
 int periodLocation = 0;
 
-foreach(string sentence in myStrings)
+for (int i = 0; i < stringsCount; i++)
 {
-  while(periodLocation != -1)
-  {
+    myString = myStrings[i];
     periodLocation = myString.IndexOf(".");
-    Console.WriteLine(sentence.Substring(periodLocation));
-    
-  }
-};
+
+    string mySentence;
+
+    // extract sentences from each string and display them one at a time
+    while (periodLocation != -1)
+    {
+
+        // first sentence is the string value to the left of the period location
+        mySentence = myString.Remove(periodLocation);
+
+        // the remainder of myString is the string value to the right of the location
+        myString = myString.Substring(periodLocation + 1);
+
+        // remove any leading white-space from myString
+        myString = myString.TrimStart();
+
+        // update the comma location and increment the counter
+        periodLocation = myString.IndexOf(".");
+
+        Console.WriteLine(mySentence);
+    }
+ 
+    mySentence = myString.Trim();
+    Console.WriteLine(mySentence);
+}
